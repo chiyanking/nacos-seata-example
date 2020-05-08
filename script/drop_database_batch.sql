f0e7ab3d3755d3bdf54bc062535c3166ad4c4877
wangtk <wangtk@yangche51.com>
2020-05-08 22:56:53 +0800

    批量删库

---
+++ script/drop_database_batch.sql
@@ -0,0 +1,33 @@
+DROP PROCEDURE IF EXISTS drop_databases;
+CREATE PROCEDURE drop_databases(
+    IN paramDBNamePrefix VARCHAR(32),
+    IN paramDBNameSuffix VARCHAR(32)
+)
+BEGIN
+    DECLARE _done INT DEFAULT 0;
+    DECLARE _cursorValue VARCHAR(255);
+
+    DECLARE _cursor CURSOR FOR SELECT SCHEMA_NAME FROM information_schema.SCHEMATA
+        WHERE SCHEMA_NAME LIKE @_paramDBName;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET _done = 1;
+
+    SET @_paramDBName = CONCAT("%", paramDBNamePrefix, "%", paramDBNameSuffix, "%");
+
+
+    OPEN _cursor;
+    REPEAT
+        FETCH _cursor INTO _cursorValue;
+        IF (_done <> 1) THEN
+            SET @prepareStatement = CONCAT("DROP DATABASE ", _cursorValue);
+            SELECT @prepareStatement;
+            PREPARE stmt FROM @prepareStatement;
+            EXECUTE stmt ;
+            DEALLOCATE PREPARE stmt;
+        END IF;
+    UNTIL _done = 1 end REPEAT;
+    CLOSE _cursor;
+ END
+
+
+call drop_databases("odb", "");
+
