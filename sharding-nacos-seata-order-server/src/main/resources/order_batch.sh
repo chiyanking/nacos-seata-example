64a1368149f601182bce383c34bb2bb27543ac02
wangtk <wangtk@yangche51.com>
2020-05-09 14:40:53 +0800

    提交代码

--- sharding-nacos-seata-order-server/src/main/resources/order_batch.sh
+++ sharding-nacos-seata-order-server/src/main/resources/order_batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #批量新建数据表
 for y in {1..5};do
-mysql -uroot -h127.0.0.1 -pdianji888 -e "use seata-order-$y; SOURCE ./order.sql;"
+mysql -uroot -h127.0.0.1 -pdianji888 -e "use seata-order-$y; SOURCE ./seata-order.sql;"
 done
