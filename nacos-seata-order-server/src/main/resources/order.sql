ea2ee901b188e892d938ba6bc5d712b313f7d78c
wangtk <wangtk@yangche51.com>
2020-05-09 09:17:02 +0800

    修改 oder.sql

--- nacos-seata-order-server/src/main/resources/order.sql
+++ nacos-seata-order-server/src/main/resources/order.sql
@@ -1,26 +1,18 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         :  本地
- Source Server Type    : MySQL
- Source Server Version : 50721
- Source Host           : 192.168.1.53:8848
- Source Schema         : db_order
-
- Target Server Type    : MySQL
- Target Server Version : 50721
- File Encoding         : 65001
-
- Date: 25/11/2019 15:11:59
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for order
--- ----------------------------
-DROP TABLE IF EXISTS `order`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `order` (
   `id` int(20) NOT NULL AUTO_INCREMENT COMMENT '主键Id',
   `user_id` int(20) DEFAULT NULL COMMENT '用户Id',
