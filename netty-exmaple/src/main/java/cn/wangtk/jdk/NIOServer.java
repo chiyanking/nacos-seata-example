afe6a15c82340c4d461d50b423c59fd07285c569
wangtk <wangtk@yangche51.com>
2020-06-25 12:10:58 +0800

    默认实现netty方法

---
+++ netty-exmaple/src/main/java/cn/wangtk/jdk/NIOServer.java
@@ -0,0 +1,79 @@
+package cn.wangtk.jdk;
+
+import lombok.SneakyThrows;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+public class NIOServer {
+
+    Selector selector;
+
+    @SneakyThrows
+    public void initServer(int port) {
+        // 获得一个ServerSocket通道
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+
+        serverChannel.configureBlocking(false);
+        serverChannel.socket().bind(new InetSocketAddress(port));
+        this.selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+    }
+
+    @SneakyThrows
+    public void listen() {
+        System.out.println("start server");
+        while (true) {
+            // 当注册事件到达时，方法返回，否则该方法会一直阻塞
+            selector.select();
+            // 获得selector中选中的相的迭代器，选中的相为注册的事件
+            Iterator ite = this.selector.selectedKeys().iterator();
+            while (ite.hasNext()) {
+                SelectionKey key = (SelectionKey) ite.next();
+                // 删除已选的key 以防重负处理
+                ite.remove();
+                // 客户端请求连接事件
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                    // 获得和客户端连接的通道
+                    SocketChannel channel = server.accept();
+                    // 设置成非阻塞
+                    channel.configureBlocking(false);
+                    // 在这里可以发送消息给客户端
+                    channel.write(ByteBuffer.wrap(new String("hello client").getBytes()));
+                    // 在客户端 连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限
+                    channel.register(this.selector, SelectionKey.OP_READ);
+                    // 获得了可读的事件
+                }else if (key.isReadable()){
+                    read(key);
+                }
+            }
+        }
+    }
+
+    @SneakyThrows
+    private void read(SelectionKey key) {
+        // 服务器可读消息，得到事件发生的socket通道
+        SocketChannel channel = (SocketChannel) key.channel();
+        // 穿件读取的缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        channel.read(buffer);
+        byte[] data = buffer.array();
+        String msg = new String(data).trim();
+        System.out.println("server receive from client: " + msg);
+        ByteBuffer outBuffer = ByteBuffer.wrap(msg.getBytes());
+        channel.write(outBuffer);
+    }
+
+    public static void main(String[] args) {
+        NIOServer server = new NIOServer();
+        server.initServer(8989);
+        server.listen();
+    }
+
+}
