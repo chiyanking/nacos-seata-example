7c2d9d671c0042c6a61fc2fba80101a716009a15
wangtk <wangtk@yangche51.com>
2020-06-29 21:27:31 +0800

    增加netty

--- netty-exmaple/src/main/java/cn/wangtk/netty/DiscardServer.java
+++ netty-exmaple/src/main/java/cn/wangtk/netty/DiscardServer.java
@@ -18,7 +18,7 @@ public class DiscardServer {
     }
 
     public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // (1)
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             ServerBootstrap bootstrap = new ServerBootstrap(); // (2)
