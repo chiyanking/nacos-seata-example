1b618b353ddb5a4fa01e1c88352bae4a72c1dc4c
wangtk <wangtk@yangche51.com>
2020-08-20 09:37:57 +0800

    增加web-security学习

---
+++ spring-boot-security/src/main/java/com/wangtk/security/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.wangtk.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+    @GetMapping("login")
+    public String login() {
+        return "";
+    }
+
+}
