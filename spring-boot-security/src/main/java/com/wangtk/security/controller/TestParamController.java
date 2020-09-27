208de7ef871cf990af96f326aa2dadef1d07c412
wangtk <wangtk@yangche51.com>
2020-09-27 20:30:27 +0800

    成为controller

--- spring-boot-security/src/main/java/com/wangtk/security/controller/TestParamController.java
+++ spring-boot-security/src/main/java/com/wangtk/security/controller/TestParamController.java
@@ -1,6 +1,7 @@
 package com.wangtk.security.controller;
 
 import com.wangtk.security.dto.WParameter;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.Date;
 
-@RestController
+@Controller
 public class TestParamController {
 
     @Resource
