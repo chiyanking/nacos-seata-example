d17b9214849257517f425c8d62f4acac54246140
wangtk <wangtk@yangche51.com>
2020-09-27 20:28:35 +0800

    测试返回类型

--- spring-boot-security/src/main/java/com/wangtk/security/controller/TestParamController.java
+++ spring-boot-security/src/main/java/com/wangtk/security/controller/TestParamController.java
@@ -2,6 +2,7 @@ package com.wangtk.security.controller;
 
 import com.wangtk.security.dto.WParameter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -17,6 +18,18 @@ public class TestParamController {
     public String testParam(WParameter wParameter) {
         System.out.println(wParameter);
         return new Date().toString();
+    }
+
+    @RequestMapping("testResponse")
+    public WParameter testResponse() {
+        System.out.println("testResponse");
+        return new WParameter();
+    }
+
+    @GetMapping("testResponseBody")
+    public WParameter testResponseBody() {
+        System.out.println("testResponseBody");
+        return new WParameter();
     }
 
 }
