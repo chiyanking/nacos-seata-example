b6de375dabe68ab14809d507b9daaceb1c8149a4
wangtk <wangtk@yangche51.com>
2020-10-27 10:57:30 +0800

    字符串回溯算法

--- spring-boot-mybatis/src/main/java/com/wangtk/mybatis/algorithm/LetterCombinations.java
+++ spring-boot-mybatis/src/main/java/com/wangtk/mybatis/algorithm/LetterCombinations.java
@@ -7,10 +7,11 @@ public class LetterCombinations {
     public static void main(String[] args) {
 
     }
+
     public List<String> letterCombinations(String digits) {
-        List<String>list=new ArrayList<>();
+        List<String> list = new ArrayList<>();
         String[] s = new String[digits.length()];
-        for(int i=0;i<digits.length();i++){
+        for (int i = 0; i < digits.length(); i++) {
             switch (digits.charAt(i)){
                 case '2':s[i]="abc";break;
                 case '3':s[i]="def";break;
@@ -22,19 +23,20 @@ public class LetterCombinations {
                 case '9':s[i]="wxyz";break;
             }
         }
-        list=getStringWithFor(s,0,list,"");
+        list = getStringWithFor(s, 0, list, "");
         return list;
     }
-    private static List<String> getStringWithFor(String []s,int i,List<String> list,String temp) {
 
-        if(i<s.length-1){
-            for(int j=0;j<s[i].length();j++){
-                list=getStringWithFor(s,i+1,list,temp+s[i].charAt(j));
+    private static List<String> getStringWithFor(String[] s, int i, List<String> list, String temp) {
+
+        if (i < s.length - 1) {
+            for (int j = 0; j < s[i].length(); j++) {
+                list = getStringWithFor(s, i + 1, list, temp + s[i].charAt(j));
             }
             i++;
         } else {
-            for(int j=0;j<s[i].length();j++){
-                list.add(temp+s[i].charAt(j));
+            for (int j = 0; j < s[i].length(); j++) {
+                list.add(temp + s[i].charAt(j));
             }
         }
 
