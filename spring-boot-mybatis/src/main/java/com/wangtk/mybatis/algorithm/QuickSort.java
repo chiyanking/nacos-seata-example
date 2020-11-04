3cb908de696d63a3c4a6281345ce0778d5855f6f
wangtk <wangtk@yangche51.com>
2020-11-04 09:17:06 +0800

    快速排序

--- spring-boot-mybatis/src/main/java/com/wangtk/mybatis/algorithm/QuickSort.java
+++ spring-boot-mybatis/src/main/java/com/wangtk/mybatis/algorithm/QuickSort.java
@@ -15,8 +15,12 @@ public class QuickSort {
     /**
      * 2, 3, 4, 9, 1, 6, 4, 8, 7, 5
      * l                          r
-     * l        r
-     * 2, 3, 4, 9, 1, 6, 4, 8, 7, 5
+     *          l                 r
+     * 2, 3, 4, 5, 1, 6, 4, 8, 7, 9
+     *          l        r
+     * 2, 3, 4, 4, 1, 6, 5, 8, 7, 9
+     *                l  r
+     * 2, 3, 4, 4, 1, 5, 6, 8, 7, 9
      */
     public static void sort(int[] arr, int left, int right) {
         if (left >= right) {
@@ -26,8 +30,13 @@ public class QuickSort {
         int rit = right;
         int base = arr[rit];
         while (let < rit) {
-            while (let < rit && arr[let] < base) {
+            while (let < rit && arr[let] <= base) {
                 let++;
+            }
+            if (let < rit) {
+                int temp = arr[let];
+                arr[let] = arr[rit];
+                arr[rit] = temp;
             }
             while (let < rit && arr[rit] > base) {
                 rit--;
